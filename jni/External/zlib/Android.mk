7ec6a60e56ceb622fb4b3c4c423e21993ddcae46
Boyia <2512854007@qq.com>
2018-09-01 00:05:28 +0800

    上传zlib等src的c文件

--- jni/External/zlib/Android.mk
+++ jni/External/zlib/Android.mk
@@ -4,25 +4,25 @@ LOCAL_PATH:= $(call my-dir)
 # and the host (as a static library for tools to use).
 
 include $(CLEAR_VARS)
-#LOCAL_SRC_FILES := $(addprefix src/,$(CSOURCES))
+#LOCAL_SRC_FILES := $(addprefix source/,$(CSOURCES))
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include/
 
 LOCAL_SRC_FILES += \
-	$(LOCAL_PATH)/src/adler32.c \
-	$(LOCAL_PATH)/src/compress.c \
-	$(LOCAL_PATH)/src/crc32.c \
-	$(LOCAL_PATH)/src/deflate.c \
-	$(LOCAL_PATH)/src/gzclose.c \
-	$(LOCAL_PATH)/src/gzlib.c \
-	$(LOCAL_PATH)/src/gzread.c \
-	$(LOCAL_PATH)/src/gzwrite.c \
-	$(LOCAL_PATH)/src/infback.c \
-	$(LOCAL_PATH)/src/inffast.c \
-	$(LOCAL_PATH)/src/inflate.c \
-	$(LOCAL_PATH)/src/inftrees.c \
-	$(LOCAL_PATH)/src/trees.c \
-	$(LOCAL_PATH)/src/uncompr.c \
-	$(LOCAL_PATH)/src/zutil.c \
+	$(LOCAL_PATH)/source/adler32.c \
+	$(LOCAL_PATH)/source/compress.c \
+	$(LOCAL_PATH)/source/crc32.c \
+	$(LOCAL_PATH)/source/deflate.c \
+	$(LOCAL_PATH)/source/gzclose.c \
+	$(LOCAL_PATH)/source/gzlib.c \
+	$(LOCAL_PATH)/source/gzread.c \
+	$(LOCAL_PATH)/source/gzwrite.c \
+	$(LOCAL_PATH)/source/infback.c \
+	$(LOCAL_PATH)/source/inffast.c \
+	$(LOCAL_PATH)/source/inflate.c \
+	$(LOCAL_PATH)/source/inftrees.c \
+	$(LOCAL_PATH)/source/trees.c \
+	$(LOCAL_PATH)/source/uncompr.c \
+	$(LOCAL_PATH)/source/zutil.c \
 
 common_CFLAGS := -std=gnu89 -Wno-unused-parameter
 LOCAL_CLANG := true
