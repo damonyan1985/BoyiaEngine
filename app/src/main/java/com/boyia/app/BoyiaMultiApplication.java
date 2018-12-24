40856126a427c441e3ec81f1d45eae67a97443ca
yanbo.boyia <2512854007@qq.com>
2018-12-24 20:41:39 +0800

    删除maven库

--- app/src/main/java/com/boyia/app/BoyiaMultiApplication.java
+++ app/src/main/java/com/boyia/app/BoyiaMultiApplication.java
@@ -1,5 +1,7 @@
 package com.boyia.app;
 
+import com.boyia.app.common.utils.BoyiaLog;
+import com.boyia.app.common.utils.ProcessUtil;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 import com.boyia.app.common.BoyiaApplication;
@@ -11,6 +13,8 @@ public class BoyiaMultiApplication extends BoyiaApplication {
     public void onCreate() {
         super.onCreate();
         mWatcher = LeakCanary.install(this);
+        // Multi Process share the same Application
+        BoyiaLog.d("BoyiaApp", "BoyiaProcess="+ProcessUtil.getCurrentProcessName(this));
     }
 
     public RefWatcher watcher() {
