a5f8e290acdb90bdc3ff93b65d4649918fa4306b
yanbo.boyia <2512854007@qq.com>
2019-01-04 23:45:24 +0800

    添加混淆配置，以及部分新创建的更新程序

---
+++ app/src/main/java/com/boyia/app/update/DownloadDAO.java
@@ -0,0 +1,49 @@
+package com.boyia.app.update;
+
+import com.boyia.app.common.db.BoyiaDAO;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import java.util.List;
+
+public class DownloadDAO extends BoyiaDAO<DownloadData> {
+    public static final String TABLE_NAME = "downloads";
+    public DownloadDAO(SQLiteDatabase db) {
+        super(db);
+    }
+
+    public DownloadData setDownloadInfo(Cursor cursor) {
+        return super.setBeanData(cursor, DownloadData.class);
+    }
+
+    private ContentValues setContentValues(DownloadData info) {
+        return super.setDbData(info, DownloadData.class);
+    }
+
+    public long insert(DownloadData info) {
+        return super.insert(info, DownloadData.class);
+    }
+
+    public void update(DownloadData info) {
+        super.update(info, info.getId(), DownloadData.class);
+    }
+
+    public void delete(int id) {
+        super.delete(id);
+    }
+
+    public DownloadData queryById(int id) {
+        return super.queryById(id, DownloadData.class);
+    }
+
+    public List<DownloadData> queryAll() {
+        return super.queryAll(DownloadData.class);
+    }
+
+    @Override
+    public String getTableName() {
+        return TABLE_NAME;
+    }
+}
+
---
+++ app/src/main/java/com/boyia/app/update/Downloader.java
@@ -0,0 +1,120 @@
+package com.boyia.app.update;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import com.boyia.app.common.loader.BoyiaLoader;
+import com.boyia.app.common.loader.ILoaderCallback;
+import com.boyia.app.common.utils.BoyiaFileUtil;
+import com.boyia.app.common.utils.BoyiaLog;
+
+
+/*
+ * Downloader
+ * @Author Yan bo
+ * @Time 2018-12-31
+ * @Copyright Reserved
+ * @Descrption HTTP Large Resource Download Loader
+ *  Interface
+ */
+
+public class Downloader implements ILoaderCallback {
+    public static final String DOWN_LOAD_DIR = BoyiaFileUtil.getFilePathRoot() + "download/";
+    private BoyiaLoader mLoader;
+    private DownloadData mInfo;
+    private RandomAccessFile mSavedFile;
+
+    public Downloader() {
+    }
+
+    public void download(DownloadData info) {
+        mInfo = info;
+        mLoader = new BoyiaLoader(this, true);
+        BoyiaLog.d("yanbo", "download url="+info.getFile_url());
+        mLoader.load(info.getFile_url());
+    }
+
+    @Override
+    public void onLoaderStart() {
+        long rangeStart = 0;
+        File downDir = new File(Downloader.DOWN_LOAD_DIR);
+        if (!downDir.exists()) {
+            downDir.mkdirs();
+        }
+
+        File downFile = new File(mInfo.getFile_path());
+        if (downFile.exists()) {
+            rangeStart = downFile.length();
+        }
+
+        if (mInfo.getStatus() == DownloadData.ERROR) {
+            rangeStart = 0;
+        }
+
+        // 断点续传
+        if (rangeStart != 0) {
+            mLoader.putHeader("Range", "bytes="+rangeStart+"-");
+        }
+
+        try {
+            mSavedFile = new RandomAccessFile(mInfo.getFile_path(), "rw");
+            mSavedFile.seek(rangeStart);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        // 正式开始启动下载
+        mInfo.setStatus(DownloadData.DOWNLOADING);
+    }
+
+    @Override
+    public void onLoaderDataSize(long size) {
+        BoyiaLog.d("yanbo", "download size="+size);
+        mInfo.setMax_len(size);
+    }
+
+    @Override
+    public void onLoaderDataReceive(byte[] data, int length) {
+        try {
+            mSavedFile.write(data, 0, length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onLoaderFinished(byte[] data, Object msg) {
+        try {
+            mSavedFile.close();
+            mInfo.setStatus(DownloadData.FINISHED);
+            DownloadUtil.updateDownloadInfo(mInfo);
+            BoyiaLog.d("yanbo", "download onLoaderFinished");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mLoader = null;
+    }
+
+    @Override
+    public void onLoaderError(String error, Object msg) {
+        if (mSavedFile != null) {
+            try {
+                mSavedFile.close();
+            } catch (IOException ioe) {
+                ioe.printStackTrace();
+            }
+
+            mInfo.setStatus(DownloadData.ERROR);
+            DownloadUtil.updateDownloadInfo(mInfo);
+        }
+
+        mLoader = null;
+    }
+
+    @Override
+    public void onLoaderRedirectUrl(String redirectUrl) {
+    }
+
+}
