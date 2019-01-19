df3f4c0bf552b93c822288c746bba8c5618a8cb4
yanbo.boyia <2512854007@qq.com>
2019-01-19 11:51:56 +0800

    适配伪TCP类，并编译通过

--- core/src/main/jni/Boyia/graphics/utils/SystemUtil.h
+++ core/src/main/jni/Boyia/graphics/utils/SystemUtil.h
@@ -10,8 +10,8 @@ class SystemUtil
 public:
 	static long getSystemTime();
 	static int intCeil(int dividend, int divid);
-	bool timeIsBetween(LUint32 later, LUint32 middle, LUint32 earlier);
-	LInt32 timeDiff(LUint32 later, LUint32 earlier);
+	static bool timeIsBetween(LUint32 later, LUint32 middle, LUint32 earlier);
+	static LInt32 timeDiff(LUint32 later, LUint32 earlier);
 };
 }
 #endif
