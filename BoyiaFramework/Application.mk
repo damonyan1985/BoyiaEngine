f782c9939fe88989d136310d88eb98aab8b9b69a
yanbo.boyia <2512854007@qq.com>
2019-11-03 22:12:59 +0800

    [fix]: optimize build tool

--- BoyiaFramework/Application.mk
+++
@@ -1,9 +0,0 @@
-APP_PLATFORM := android-19
-APP_ABI      := arm64-v8a
-#APP_ABI      := armeabi-v7a
-#APP_ABI      := armeabi-v7a
-APP_SHORT_COMMANDS := true
-APP_CFLAGS   += -Wno-error=format-security
-APP_CPPFLAGS := -frtti -std=c++11
-APP_STL      := stlport_static
-NDK_TOOLCHAIN_VERSION=4.9
