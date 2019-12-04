c2fbcee13c58653d2a63e4539fe36252b7165893
yanbo.boyia <2512854007@qq.com>
2019-12-04 09:50:22 +0800

    [feat]: Test PlatformBridgeWin

--- BoyiaFramework/source/boyia/kernal/platform/win/PlatformBridgeWin.cpp
+++ BoyiaFramework/source/boyia/kernal/platform/win/PlatformBridgeWin.cpp
@@ -1,9 +1,18 @@
 #include "PlatformBridge.h"
 #include "SalLog.h"
+#if ENABLE(BOYIA_WINDOWS)
+#include <windows.h>
+#include <ShlObj.h>
+
+#define CSIDL_LOCAL_APPDATA             0x001c
+#define MAX_PATH          260
 
 namespace yanbo {
+static String sAppPath((LUint8)0, MAX_PATH);
+static String sBoyiaJsonPath((LUint8)0, MAX_PATH);
+static String sSdkPath((LUint8)0, MAX_PATH);
+static String sAppRootPath((LUint8)0, MAX_PATH);
 
-#if ENABLE(BOYIA_WINDOWS)
 bool PlatformBridge::unzip(const String& zipFile, const String& dir)
 {
     return false;
@@ -11,28 +20,43 @@ bool PlatformBridge::unzip(const String& zipFile, const String& dir)
 
 const char* PlatformBridge::getAppPath()
 {
-    return "";
+    if (!sAppPath.GetLength()) {
+        sAppPath = _CS(getAppRoot());
+        sAppPath += _CS("\\apps\\");
+    }
+    return GET_STR(sAppPath);
 }
 
 const char* PlatformBridge::getBoyiaJsonPath()
 {
-    return "";
+    if (!sBoyiaJsonPath.GetLength()) {
+        sBoyiaJsonPath = _CS(getAppRoot());
+        sBoyiaJsonPath += _CS("\\boyia.json");
+    }
+    return GET_STR(sBoyiaJsonPath);
 }
 
 const char* PlatformBridge::getSdkPath()
 {
-    return "";
+    if (!sSdkPath.GetLength()) {
+        sSdkPath = _CS(getAppRoot());
+        sSdkPath += _CS("\\sdk");
+    }
+    return GET_STR(sSdkPath);
 }
 
 const char* PlatformBridge::getAppRoot()
 {
-    return "";
+    if (!sAppRootPath.GetLength()) {
+        ::SHGetSpecialFolderPathA(NULL, (char*)sAppRootPath.GetBuffer(), CSIDL_LOCAL_APPDATA, 0);
+    }
+    return GET_STR(sAppRootPath);
 }
 
 const char* PlatformBridge::getBoyiaJsonUrl()
 {
     return "";
 }
+}
 
 #endif
-}
