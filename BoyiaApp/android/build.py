c1e27d0cdabcb48def5bd9eb8f56560c2d911d54
yanbo.boyia <2512854007@qq.com>
2019-12-14 21:07:19 +0800

    [fix]: fixed library compile

---
+++ BoyiaApp/android/build.py
@@ -0,0 +1,44 @@
+import os
+import operator
+
+android_dir = os.getcwd()
+maven_dir = os.path.join(android_dir, 'maven')
+maven_project_dir = os.path.join(maven_dir, 'BoyiaMaven')
+core_dir = os.path.join(android_dir, 'core')
+
+pull_core_maven_cmd = (
+  'cd maven && git clone git@github.com:damonyan1985/BoyiaMaven.git'
+)
+
+upload_core_library_cmd = (
+  'cd core && gradle :core:uploadArchives'
+)
+
+def del_file(path, isDel):
+  ls = os.listdir(path)
+  for i in ls:
+    c_path = os.path.join(path, i)
+    print("i=" + i)
+    if operator.eq(i, '.git') == True:
+      continue
+    if operator.eq(i, '.gitignore') == True:
+      continue
+    print("c_path=" + c_path);  
+    if os.path.isdir(c_path):
+      del_file(c_path, True)
+    else:
+      os.remove(c_path)
+  if isDel:      
+    os.rmdir(path)   
+
+def main():
+  print('maven_dir=' + maven_dir)
+  if os.path.exists(maven_dir) == False:
+    os.mkdir(maven_dir)
+    os.system(pull_core_maven_cmd)
+  del_file(maven_project_dir, False)
+  os.system(upload_core_library_cmd)
+  print('Build Finished!')
+
+if __name__ == '__main__':
+  main()
