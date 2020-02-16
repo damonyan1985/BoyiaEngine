5a31b22f83065bdeb1b85471ee9f817db4c12bd0
yanbo.boyia <2512854007@qq.com>
2020-02-16 12:32:36 +0800

    [fix]: add support cert

--- BoyiaApp/android/loader/src/main/java/com/boyia/app/loader/http/SSLHelper.java
+++ BoyiaApp/android/loader/src/main/java/com/boyia/app/loader/http/SSLHelper.java
@@ -1,21 +1,59 @@
 package com.boyia.app.loader.http;
 
+import android.content.res.AssetManager;
+
+import com.boyia.app.common.BaseApplication;
+
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.security.KeyStore;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
+import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
 import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 import javax.net.ssl.X509TrustManager;
 
 public class SSLHelper {
+    private static final String CERT_PATH = "cert";
+    private static final String KEY_STORE_PASSWORD = "";
+    private static final String KEY_STORE_TRUST_PASSWORD = "";
+
     public static SSLInfo getSSLInfo() {
         return new SSLInfo();
+    }
+
+    public static TrustManagerFactory getCertificates() {
+        try {
+            AssetManager assets = BaseApplication.getInstance().getAssets();
+            String certs[] = assets.list(CERT_PATH);
+            if (certs == null || certs.length == 0) {
+                return null;
+            }
+
+            KeyStore keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            keyStore.load(null);
+            CertificateFactory factory = CertificateFactory.getInstance("X.509");
+            for (int i = 0; i < certs.length; ) {
+                InputStream input = assets.open(certs[i]);
+                keyStore.setCertificateEntry(Integer.toString(i++), factory.generateCertificate(input));
+                input.close();
+            }
+
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(keyStore);
+            return tmf;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
     }
 
     public static class SSLInfo {
@@ -25,7 +63,8 @@ public class SSLHelper {
         public SSLInfo() {
             try {
                 SSLContext context = SSLContext.getInstance("TLS");
-                mTrustManager = new X509TrustManager() {
+                TrustManagerFactory factory = getCertificates();
+                mTrustManager = factory != null ? (X509TrustManager) factory.getTrustManagers()[0] :  new X509TrustManager() {
                     @Override
                     public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
                     }
@@ -34,13 +73,18 @@ public class SSLHelper {
                     public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
                     }
 
+                    // 返回授信证书列表
                     @Override
                     public X509Certificate[] getAcceptedIssuers() {
                         //return new X509Certificate[0];
                         return new X509Certificate[]{};
                     }
                 };
-                context.init(null, new TrustManager[] { mTrustManager }, new SecureRandom());
+                context.init(
+                        null,
+                        factory != null ? factory.getTrustManagers() : new TrustManager[] { mTrustManager },
+                        new SecureRandom()
+                );
 
                 //return context.getSocketFactory();
                 mFactory =  new TlsCompatSocketFactory(context.getSocketFactory());
