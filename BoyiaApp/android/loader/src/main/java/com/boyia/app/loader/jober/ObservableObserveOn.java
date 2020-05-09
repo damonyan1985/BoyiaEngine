45ce6fbc0cb741d3a82350aa1696c3cb3f5eb77b
yanbo.boyia <2512854007@qq.com>
2020-05-09 12:30:21 +0800

    [comment]: edit comments

--- BoyiaApp/android/loader/src/main/java/com/boyia/app/loader/jober/ObservableObserveOn.java
+++ BoyiaApp/android/loader/src/main/java/com/boyia/app/loader/jober/ObservableObserveOn.java
@@ -12,10 +12,12 @@ public class ObservableObserveOn<T> extends Observable<T> {
     }
 
     // 1. 如果由observeOn创建的Observable执行此方法，则mSource为subscribeOn创建的Observable
-    // 2. 如果由subscribeOn创建的Observable执行此方法，则mSource为ObservableCreate
-    // 3. ObservableCreate执行subscribe，会执行Subscriber中的内容
-    // 4. Subscriber执行包装了传入的Subscriber对象的EventEmitter
-    // 5. EventEmitter相关方法执行会回调传入的Subscriber中对应的方法
+    // 2. 由subscribeOn创建的Observable为ObservableSubscribeOn类对象执行此方法
+    // 3，ObservableSubscribeOn对象执行subscribe时会先执行schedule，然后调用ObservableCreate.subscribe
+    // 4. ObservableCreate执行subscribe，传入ObservableObserveOn包装的Subscriber
+    // 5. ObservableCreate.subscribe执行包装了传入的Subscriber对象的EventEmitter
+    // 6. EventEmitter相关方法执行会回调传入的Subscriber中对应的方法
+    // 7. 最终会执行ObservableObserveOn的mScheduler中
     @Override
     void subscribeActual(Subscriber<? super T> observer) {
         mSource.subscribe(new Subscriber<T>() {
