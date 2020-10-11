368b2c41fbec2dd4761e15210504c527f11fe7e4
yanbo.boyia <2512854007@qq.com>
2020-10-11 10:15:37 +0800

    [optimize]: format code

--- BoyiaFramework/source/boyia/kernel/platform/win/BoyiaHttpEngine.cpp
+++ BoyiaFramework/source/boyia/kernel/platform/win/BoyiaHttpEngine.cpp
@@ -48,31 +48,31 @@ LVoid BoyiaHttpEngine::request(const String& url, LInt method)
     }
 
     // 解析Url
-	wstring wurl = CharConvertor::CharToWchar(GET_STR(url));
-	Uri uri;
-	UrlParser::parse(wurl, uri);
+    wstring wurl = CharConvertor::CharToWchar(GET_STR(url));
+    Uri uri;
+    UrlParser::parse(wurl, uri);
 
     // 初始化wininet
-	HINTERNET internet = ::InternetOpen(L"WinInetGet/0.1", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+    HINTERNET internet = ::InternetOpen(L"WinInetGet/0.1", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
     BOYIA_LOG("BoyiaHttpEngine---request Open Internet %d", 1);
 
     // 建立连接
-	DWORD dwConnectContext = 0;
-	HINTERNET connect = ::InternetConnect(internet, uri.host.c_str(), INTERNET_DEFAULT_HTTPS_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, dwConnectContext);
+    DWORD dwConnectContext = 0;
+    HINTERNET connect = ::InternetConnect(internet, uri.host.c_str(), INTERNET_DEFAULT_HTTPS_PORT, NULL, NULL, INTERNET_SERVICE_HTTP, 0, dwConnectContext);
 
-	DWORD dwOpenRequestFlags = INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTP |
-		INTERNET_FLAG_KEEP_CONNECTION |
-		INTERNET_FLAG_NO_AUTH |
-		INTERNET_FLAG_NO_COOKIES |
-		INTERNET_FLAG_NO_UI |
-		// Set https
-		INTERNET_FLAG_SECURE |
-		INTERNET_FLAG_RELOAD;
+    DWORD dwOpenRequestFlags = INTERNET_FLAG_IGNORE_REDIRECT_TO_HTTP |
+	    INTERNET_FLAG_KEEP_CONNECTION |
+	    INTERNET_FLAG_NO_AUTH |
+	    INTERNET_FLAG_NO_COOKIES |
+	    INTERNET_FLAG_NO_UI |
+	    // Set https
+	    INTERNET_FLAG_SECURE |
+	    INTERNET_FLAG_RELOAD;
 
     // 打开请求
-	HINTERNET request = HttpOpenRequest(connect, method == NetworkBase::GET ? L"GET" : L"POST", uri.path.c_str(), NULL,
-		NULL, NULL,
-		dwOpenRequestFlags, dwConnectContext);
+    HINTERNET request = HttpOpenRequest(connect, method == NetworkBase::GET ? L"GET" : L"POST", uri.path.c_str(), NULL,
+	    NULL, NULL,
+	    dwOpenRequestFlags, dwConnectContext);
 
     // 设置HTTP请求头
     if (m_header.GetLength() > 0) {
@@ -89,55 +89,55 @@ LVoid BoyiaHttpEngine::request(const String& url, LInt method)
     }
 
     // 发送请求数据
-	DWORD dwError = 0;
+    DWORD dwError = 0;
     LByte* dataBuffer = m_data.get() ? m_data->GetBuffer() : NULL;
     LInt dataSize = m_data.get() ? m_data->GetLength() : 0;
-	if (!HttpSendRequest(request, NULL, 0, dataBuffer, dataSize)) {
-		dwError = GetLastError();
-		BOYIA_LOG("HttpSendRequest error: ", dwError);
+    if (!HttpSendRequest(request, NULL, 0, dataBuffer, dataSize)) {
+        dwError = GetLastError();
+        BOYIA_LOG("HttpSendRequest error: ", dwError);
         m_callback->onLoadError(NetworkClient::kNetworkFileError);
         return;
-	}
+    }
 
-	if (dwError == ERROR_INTERNET_INVALID_CA) {
-		fprintf(stderr, "HttpSendRequest failed, error: %d (0x%x)/n",
-			dwError, dwError);
+    if (dwError == ERROR_INTERNET_INVALID_CA) {
+        fprintf(stderr, "HttpSendRequest failed, error: %d (0x%x)/n",
+	        dwError, dwError);
 
-		DWORD dwFlags;
-		DWORD dwBuffLen = sizeof(dwFlags);
-		InternetQueryOption(request, INTERNET_OPTION_SECURITY_FLAGS,
-			(LPVOID)& dwFlags, &dwBuffLen);
+        DWORD dwFlags;
+        DWORD dwBuffLen = sizeof(dwFlags);
+        InternetQueryOption(request, INTERNET_OPTION_SECURITY_FLAGS,
+	        (LPVOID)& dwFlags, &dwBuffLen);
 
-		dwFlags |= SECURITY_FLAG_IGNORE_UNKNOWN_CA;
-		InternetSetOption(request, INTERNET_OPTION_SECURITY_FLAGS,
-			&dwFlags, sizeof(dwFlags));
+        dwFlags |= SECURITY_FLAG_IGNORE_UNKNOWN_CA;
+        InternetSetOption(request, INTERNET_OPTION_SECURITY_FLAGS,
+	        &dwFlags, sizeof(dwFlags));
 
-		request = HttpOpenRequest(connect, method == NetworkBase::GET ? L"GET" : L"POST", uri.path.c_str(), NULL,
-			NULL, NULL,
-			dwOpenRequestFlags, dwConnectContext);
-	}
+        request = HttpOpenRequest(connect, method == NetworkBase::GET ? L"GET" : L"POST", uri.path.c_str(), NULL,
+	        NULL, NULL,
+	        dwOpenRequestFlags, dwConnectContext);
+    }
 
-	// Get HTTP Response Header
-	DWORD dwInfoLevel = HTTP_QUERY_RAW_HEADERS_CRLF;
-	DWORD dwInfoBufferLength = 2048;
-	BYTE* pInfoBuffer = (BYTE*)malloc(dwInfoBufferLength + 2);
-	while (!HttpQueryInfo(request, dwInfoLevel, pInfoBuffer, &dwInfoBufferLength, NULL)) {
-		DWORD dwError = GetLastError();
-		if (dwError == ERROR_INSUFFICIENT_BUFFER) {
-			free(pInfoBuffer);
-			pInfoBuffer = (BYTE*)malloc(dwInfoBufferLength + 2);
-		} else {
-			fprintf(stderr, "HttpQueryInfo failed, error: %d (0x%x)/n",
-				GetLastError(), GetLastError());
-			break;
-		}
-	}
-	pInfoBuffer[dwInfoBufferLength] = '/0';
-	pInfoBuffer[dwInfoBufferLength + 1] = '/0';
-	printf("%s", pInfoBuffer);
-	free(pInfoBuffer);
+    // Get HTTP Response Header
+    DWORD dwInfoLevel = HTTP_QUERY_RAW_HEADERS_CRLF;
+    DWORD dwInfoBufferLength = 2048;
+    BYTE* pInfoBuffer = (BYTE*)malloc(dwInfoBufferLength + 2);
+    while (!HttpQueryInfo(request, dwInfoLevel, pInfoBuffer, &dwInfoBufferLength, NULL)) {
+        DWORD dwError = GetLastError();
+        if (dwError == ERROR_INSUFFICIENT_BUFFER) {
+            free(pInfoBuffer);
+            pInfoBuffer = (BYTE*)malloc(dwInfoBufferLength + 2);
+        } else {
+            fprintf(stderr, "HttpQueryInfo failed, error: %d (0x%x)/n",
+	            GetLastError(), GetLastError());
+            break;
+        }
+    }
+    pInfoBuffer[dwInfoBufferLength] = '/0';
+    pInfoBuffer[dwInfoBufferLength + 1] = '/0';
+    printf("%s", pInfoBuffer);
+    free(pInfoBuffer);
 	// Get Response Body
-	DWORD bufferSize = 1024;
+    DWORD bufferSize = 1024;
     LByte* buffer = new LByte[bufferSize];
     /*
 	while (InternetQueryDataAvailable(request, &dwBytesAvailable, 0, 0)) {
@@ -175,9 +175,9 @@ LVoid BoyiaHttpEngine::request(const String& url, LInt method)
     m_callback->onLoadFinished();
 
     // Close Http resource
-	InternetCloseHandle(request);
-	InternetCloseHandle(connect);
-	InternetCloseHandle(internet);
+    InternetCloseHandle(request);
+    InternetCloseHandle(connect);
+    InternetCloseHandle(internet);
 
     BOYIA_LOG("BoyiaHttpEngine---request end %d", 2);
 }
