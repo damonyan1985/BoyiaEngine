c799e0c5e613ce8ea11ad3305a11adbb850dbfec
yanbo.boyia <2512854007@qq.com>
2021-01-01 10:57:48 +0800

    [fix]: format code

--- BoyiaFramework/source/boyia/kernel/platform/win/CharConvertor.cpp
+++ BoyiaFramework/source/boyia/kernel/platform/win/CharConvertor.cpp
@@ -4,25 +4,25 @@
 namespace yanbo {
 std::wstring CharConvertor::CharToWchar(const char* c, size_t encode)
 {
-	std::wstring str;
-	int len = MultiByteToWideChar(encode, 0, c, strlen(c), NULL, 0);
-	wchar_t* wstr = new wchar_t[len + 1];
-	MultiByteToWideChar(encode, 0, c, strlen(c), wstr, len);
-	wstr[len] = '\0';
-	str = wstr;
-	delete wstr;
-	return str;
+    std::wstring str;
+    int len = MultiByteToWideChar(encode, 0, c, strlen(c), NULL, 0);
+    wchar_t* wstr = new wchar_t[len + 1];
+    MultiByteToWideChar(encode, 0, c, strlen(c), wstr, len);
+    wstr[len] = '\0';
+    str = wstr;
+    delete wstr;
+    return str;
 }
 
 LVoid CharConvertor::WcharToChar(const wchar_t* wp, String& text, size_t encode)
 {
-	//std::string str;
-	int len = WideCharToMultiByte(encode, 0, wp, wcslen(wp), NULL, 0, NULL, NULL);
-	char* chs = new char[len + 1];
-	WideCharToMultiByte(encode, 0, wp, wcslen(wp), chs, len, NULL, NULL);
+    //std::string str;
+    int len = WideCharToMultiByte(encode, 0, wp, wcslen(wp), NULL, 0, NULL, NULL);
+    char* chs = new char[len + 1];
+    WideCharToMultiByte(encode, 0, wp, wcslen(wp), chs, len, NULL, NULL);
     chs[len] = '\0';
-	//str = m_char;
-	//delete m_char;
+    //str = m_char;
+    //delete m_char;
     text.Copy(_CS(chs), LTrue, len);
 }
 }
