76938f279b02e1aab18620fe8b8d016463feba3b
yanbo.boyia <2512854007@qq.com>
2021-03-03 09:27:55 +0800

    [feat]: add corner shader

--- BoyiaFramework/source/boyia/painter/shaders/FragmentSource.h
+++ BoyiaFramework/source/boyia/painter/shaders/FragmentSource.h
@@ -6,8 +6,12 @@
 namespace yanbo {
 class FragmentSource {
 public:
+    // 处理基本UI元素
     static const char* s_uielement;
+    // 处理视频
     static const char* s_video;
+    // 处理圆角矩形
+    static const char* s_corner;
 };
 } // namespace yanbo
 
