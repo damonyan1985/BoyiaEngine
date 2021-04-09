c444b0053e793eacecccb1e871f9fc6bf2f0dafc
yanbo.boyia <2512854007@qq.com>
2021-04-09 09:21:36 +0800

    [feat]: add fits screen

--- BoyiaApp/android/app/src/main/java/com/boyia/app/BoyiaActivity.java
+++ BoyiaApp/android/app/src/main/java/com/boyia/app/BoyiaActivity.java
@@ -14,15 +14,20 @@ import com.boyia.app.core.BoyiaUIView;
 import com.boyia.app.loader.image.BoyiaImager;
 import com.boyia.app.loader.job.JobScheduler;
 
+import android.annotation.TargetApi;
 import android.content.ComponentCallbacks2;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.RemoteException;
 import android.view.KeyEvent;
 import android.app.Activity;
 import android.os.Process;
+import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 public class BoyiaActivity extends Activity {
@@ -38,6 +43,11 @@ public class BoyiaActivity extends Activity {
         super.onCreate(icicle);
         mContainer = new FrameLayout(this);
         setContentView(mContainer);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        // 防止重新布局
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
         mSplashManager = new TTAdSplashManager();
         mSplashManager.loadSplashAd(this,
                 mContainer, () -> startBoyiaUI());
@@ -143,7 +153,51 @@ public class BoyiaActivity extends Activity {
         }
     }
 
+    private static void setRootView(Activity activity) {
+        ViewGroup parent = (ViewGroup) activity.findViewById(android.R.id.content);
+        for (int i = 0, count = parent.getChildCount(); i < count; i++) {
+            View childView = parent.getChildAt(i);
+            if (childView instanceof ViewGroup) {
+                childView.setFitsSystemWindows(true);
+                ((ViewGroup) childView).setClipToPadding(true);
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private static void transparentStatusBar(Activity activity) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            activity.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            //需要设置这个flag contentView才能延伸到状态栏
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            //状态栏覆盖在contentView上面，设置透明使contentView的背景透出来
+            activity.getWindow().setStatusBarColor(0xffed4040);
+        } else {
+            //让contentView延伸到状态栏并且设置状态栏颜色透明
+            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+    }
+
+    /**
+     * 设置状态栏全透明
+     *
+     * @param activity 需要设置的activity
+     */
+    public static void setTransparent(Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+            return;
+        }
+        transparentStatusBar(activity);
+        setRootView(activity);
+    }
+
+
     private void startBoyiaUI() {
+
+        setTransparent(this);
+        //getWindow().getDecorView().setFitsSystemWindows(true);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_VISIBLE);
         BoyiaCoreJNI.initLibrary(() -> {
             mContainer.removeAllViews();
             FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
