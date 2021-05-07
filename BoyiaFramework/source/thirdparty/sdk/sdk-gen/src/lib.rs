b0bd3f324857ce282766cac347e7c5ddca876529
yanbo.boyia <2512854007@qq.com>
2021-05-07 23:56:56 +0800

    [feat]: add route path in server

--- BoyiaFramework/source/thirdparty/sdk/sdk-gen/src/lib.rs
+++ BoyiaFramework/source/thirdparty/sdk/sdk-gen/src/lib.rs
@@ -1,8 +1,10 @@
 use proc_macro::TokenStream;
 
+// 本地服务端请求路由宏开发
+
 // args表示宏的参数
 // input为宏修饰的函数或者属性的token流
 #[proc_macro_attribute]
-pub fn get(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn get(_args: TokenStream, input: TokenStream) -> TokenStream {
     input
 }
--- BoyiaFramework/source/thirdparty/sdk/sdk-main/src/web/server.rs
+++ BoyiaFramework/source/thirdparty/sdk/sdk-main/src/web/server.rs
@@ -2,13 +2,33 @@ use std::thread;
 
 // hyper import
 use std::{convert::Infallible, net::SocketAddr};
-use hyper::{Body, Request, Response, Server};
+use hyper::{Body, Method, Request, Response, Server, StatusCode};
 use hyper::service::{make_service_fn, service_fn};
 use tokio::runtime::Runtime;
 
-async fn handle(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn handle(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
   info!("http handle respone");
-  Ok(Response::new("Just Test Hello, World!!!!\n".into()))
+  let mut response = Response::new(Body::empty());
+  match (req.method(), req.uri().path()) {
+    (&Method::GET, "/") => {
+      *response.body_mut() = Body::from("Try get data to /");
+    },
+    (&Method::GET, "/main") => {
+      *response.body_mut() = Body::from("Try get data to /main");
+    },
+    (&Method::GET, "/login") => {
+      *response.body_mut() = Body::from("Try get data to /login");
+    },
+    (&Method::GET, "/detail") => {
+      *response.body_mut() = Body::from("Try get data to /detail");
+    },
+    _ => {
+      *response.status_mut() = StatusCode::NOT_FOUND;
+    },
+  }
+
+  Ok(response)
+  //Ok(Response::new("Just Test Hello, World!!!!\n".into()))
 }
 
 async fn start_server() {
@@ -16,7 +36,7 @@ async fn start_server() {
 
   // 从handle创建一个服务
   let make_svc = make_service_fn(|_conn| async {
-      Ok::<_, Infallible>(service_fn(handle))
+      Ok::<_, hyper::Error>(service_fn(handle))
   });
 
   let server = Server::bind(&addr).serve(make_svc);
