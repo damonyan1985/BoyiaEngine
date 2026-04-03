a1ef9bfee9713e3713980b16638e252f4b2e0796
damonyan1985 <2512854007@qq.com>
2026-04-03 17:40:01 +0800

    [optimize]: optimize script code

--- BoyiaFramework/source/thirdparty/vm/examples/boyia_cli/src/main.rs
+++ BoyiaFramework/source/thirdparty/vm/examples/boyia_cli/src/main.rs
@@ -1,5 +1,6 @@
-//! Example: use BoyiaRuntime to compile and run a simple Boyia program.
-//! Prints results of simple calculations via BY_Log.
+//! Example: use BoyiaRuntime to compile and run a Boyia program from `script/main.boyia`
+//! (under this crate directory, resolved via `CARGO_MANIFEST_DIR`).
+//! Prints results via `BY_Log` and exercises async/File/Https builtins.
 //!
 //! If the program hangs or crashes, see CRASH_ANALYSIS.md. You can set
 //! env BOYIA_INIT_MINIMAL=1 to skip builtin classes (faster init, fewer deps)
@@ -53,152 +54,16 @@ fn main() {
     }
     println!("[3] VM ready.");
 
-    // Boyia class with a method that prints a string: Printer.say(msg) calls BY_Log(msg).
-    // Create instance with new(Printer); call p.say("hello") to print (method dispatch runs during compile/execute).
-    let script = r#"class Printer { fun say(msg) { BY_Log(msg); } };
-class MyAdd { fun add(a, b) { return a+b; } }    
-var p = new(Printer);
-fun printlog(p, str) {
-    p.say(str);
-}
-printlog(p, "hello");    
-printlog(p, "world");
-
-var a = new(MyAdd);
-printlog(p, "result:" + a.add(100,202));
-
-var arr1 = ["789", "100"];
-printlog(p, arr1.get(0));
-
-var body = {
-    "Content-Type" : "application/x-www-form-urlencoded",
-    "User-Token" : "1234567890",
-    body : "name=test&pwd=test",
-    items : ["123", "456"]
-};
-printlog(p, body.get("body"));
-
-class Util {
-    fun newMicrotask(worker) {
-        BY_Log("call newMicrotask");
-        var task = new(MicroTask);
-        BY_Log("call MicroTask create");
-        task.init(worker);
-        BY_Log("call MicroTask init");
-        return task;
-    }
-}
-
-class PrinterExt extends Printer {
-    prop fun multiply(a, b) {
-        return a*b;
-    }
-
-    prop fun load() {
-        Https.load("https://httpbin.org/get", fun(body) {
-            var result = body;
-            BY_Log("Https.load result: " + body);
-        });
-    }
-
-    prop fun demoFile() {
-        File.write("boyia_cli_demo.txt", "hello from File.write", fun(err) {
-            if (err == "ok") {
-                BY_Log("File.write ok");
-            } else {
-                BY_Log("File.write: " + err);
-            }
-            
-        });
-
-        File.read("boyia_cli_demo.txt", fun(content) {
-            BY_Log("File.read: " + content);
-        });
-    }
-
-    prop async writeFile() {
-        var result = (await this.writeFilePromise("hello from File.write"));
-        if (result == "ok") {
-            BY_Log("writeFile ok");
-        } else {
-            BY_Log("writeFile: " + result);
-        }
-
-        File.read("boyia_cli_demo.txt", fun(content) {
-            BY_Log("File.read: " + content);
-        });
-    }
-
-    prop async writeFilePromise(content) {
-        BY_Log("run writeFilePromise");
-        //var content = "hello from File.write";
-        BY_Log(content);
-        Util.newMicrotask(fun(resolve) {
-            BY_Log("run writeFilePromise");
-            File.write("boyia_cli_demo.txt", content, resolve);
-        });
-    }
-
-    prop async loadAsync() {
-        for (var i = 0; i < 10; i=i+1) {
-            if (i == 6) {
-                break;
-            }
-            BY_Log("loadAsync loop: " + i);
-        }
-    
-        var result = (await this.loadPromise());
-        var test = "hello : " + result;
-        BY_Log("loadAsync result: " + test);
-        return result;
-    }
-
-    prop async loadPromise() {
-        BY_Log("run loadPromise");
-        Util.newMicrotask(fun(resolve) {
-            BY_Log("run newMicrotask");
-            Https.load("https://httpbin.org/get", resolve);
-        });
-    }
-
-    prop fun testLocal() {
-        var test = "hello";
-        for (var i = 0; i < 10; i=i+1) {
-            var test = "world";
-            if (i == 6) {
-                break;
-            }
-            BY_Log("testLocal loop: " + i + test);
-        }
-
-        BY_Log("testLocal result: " + test);
-    }
-}
-
-var pe = new(PrinterExt);
-printlog(p, pe.multiply(30, 41));
-printlog(pe, pe.multiply(30, 42));
-BY_Log(123);
-//pe.load();
-
-pe.loadAsync();
-//pe.demoFile();
-//pe.testLocal();
-pe.writeFile();
-
-printlog(p, arr1.get(0));
-
-arr1.add("811");
-printlog(p, arr1.get(1));
-printlog(p, arr1.get(2));
-
-"#;
-//     let script = r#"class Printer { fun say(msg) { BY_Log(msg); } }
-// "#;
+    let script_path = std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("script")
+        .join("main.boyia");
+    let script = std::fs::read_to_string(&script_path).unwrap_or_else(|e| {
+        panic!("failed to read {}: {e}", script_path.display());
+    });
 
     println!("[4] Compiling script...");
     runner
-        .compile(script)
+        .compile(&script)
         .expect("failed to compile script on task thread");
     println!("[6] Running script...");
 
